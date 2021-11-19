293da5bf21de63cec68852f9d843e9fc53d9f012
Krycekr <krycekr@gmail.com>
2021-11-19 06:20:17 -0300

    iniciamos o projeto

---
+++ class/Sql.php
@@ -0,0 +1,53 @@
+<?php 
+
+	class Sql extends PDO {
+
+		private $conn;
+
+		public function __construct(){
+
+			$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","arduino123","2020@Geotec");
+
+		}
+
+		private function setParams($statment, $parameters = array()){
+
+			foreach ($parameters as $key => $value) {
+
+				$this->setParam($key,$value);
+
+			}
+
+		}
+
+
+		private function setParam($statment,$rey, $value){
+
+			$statment->bindParam($key,$value);
+
+		}
+
+		public function execQuery($rawQuery, $params = array()){
+
+			$stmt = $this->conn->prepare($rawQuery);
+
+			$this->setParams($stmt,$params);
+
+			$stmt->execute();
+
+			return $stmt;
+
+		}
+
+		public function select($rawQuery, $params = array()):array{
+
+			$stmt = $this->execQuery($rawQuery,$params);
+
+			return $stmt->fetchALL(PDO::FETCH_ASSOC);
+			
+
+		}
+
+	}
+
+ ?>
